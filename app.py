c947688cfe33ac6d62fe9252e3ad5ec4e1ecaa35
Prasanna <93439530+Prasannapro@users.noreply.github.com>
2023-09-25 15:17:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,21 @@ def home():
         # api_key: 6f5f05a0291976543db63f26234bd0a8
         weather_url=requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api}&units=metric')
 
-        weather_data=weather_url.json()
-        print(weather_data)
+    #     weather_data=weather_url.json()
+    #     print(weather_data)
 
-        temp=weather_data['main']['temp']
-        # weather=weather_data['weather']['description']
+    #     temp=weather_data['main']['temp']
+    #     # weather=weather_data['weather']['description']
 
-        return render_template("result.html",temp=temp,location=location)
+    #     return render_template("result.html",temp=temp,location=location)
+    # return render_template("index.html")
+      if(weather_data["cod"] == "404"):
+            return render_template("error.html")
+        else:
+            temp=weather_data['main']['temp']
+            weather=weather_data['weather'][0]['description']
+
+            return render_template("result.html",temp=temp,location=location,weather=weather)
     return render_template("index.html")
 
 if __name__ == "__main__":
